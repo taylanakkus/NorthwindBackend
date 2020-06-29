44a164e068211b2dfde5aa4e3a6aac1e20598394
t4yo <taylan.akkus@outlook.com>
2020-06-29 23:12:37 +0300

    24. Data access for role management

--- DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -8,11 +8,14 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
 {
 	public class NorthwindContext : DbContext
 	{
-		public DbSet<Product> Products { get; set; }
-		public DbSet<Category> Categories { get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SE7FTJ2\SQLEXPRESS01;Initial Catalog=Northwind;Integrated Security=True");
 		}
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<OperationClaim> OperationClaims { get; set; }
+		public DbSet<User> Users { get; set; }
+		public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
 	}
 }
