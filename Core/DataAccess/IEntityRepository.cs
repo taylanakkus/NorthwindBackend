e32e973bb4654c5d86976be89062803f545846db
t4yo <taylan.akkus@outlook.com>
2020-06-29 14:19:51 +0300

    Repository base is written

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -8,7 +8,7 @@ namespace Core.DataAccess
 {
 	public interface IEntityRepository<T> where T:class, IEntity, new()
 	{
-		T Get();
+		T Get(Expression<Func<T, bool>> filter);
 		IList<T> GetList(Expression<Func<T, bool>> filter = null);
 		void Add(T entity);
 		void Update(T entity);
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,36 +1,14 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-	class EfProductDal : IProductDal
+	public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
 	{
-		public void Add(Product entity)
-		{
-			throw new NotImplementedException();
-		}
 
-		public void Delete(Product entity)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Product Get()
-		{
-			throw new NotImplementedException();
-		}
-
-		public IList<Product> GetList(Expression<Func<Product, bool>> filter = null)
-		{
-			throw new NotImplementedException();
-		}
-
-		public void Update(Product entity)
-		{
-			throw new NotImplementedException();
-		}
 	}
 }
