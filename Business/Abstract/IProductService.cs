a308e79eb407a56d579208b7dee8278a13659536
t4yo <taylan.akkus@outlook.com>
2020-06-29 14:27:40 +0300

    09. Business layer implemantation has been started

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+	public interface IProductService
+	{
+		Product GetById(int productId);
+		List<Product> GetList();
+		List<Product> GetListByCategory(int categoryById);
+		void Add(Product product);
+		void Delete(Product product);
+		void Update(Product product);
+	}
+}
