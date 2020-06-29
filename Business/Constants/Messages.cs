ae7f54efdbdd9ea73fe053e27c21bfdacf0fa217
t4yo <taylan.akkus@outlook.com>
2020-06-30 01:24:20 +0300

    34. Implementation of Login method has been started

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,14 @@ namespace Business.Constants
 {
 	public static class Messages
 	{
-		public static string ProductAdded = "The product has been added successfully.";
-		public static string ProductDeleted = "The product has been deleted successfully.";
-		public static string ProductUpdated = "The product has been updated successfully.";
+		public static string ProductAdded = "The product has been added successfully";
+		public static string ProductDeleted = "The product has been deleted successfully";
+		public static string ProductUpdated = "The product has been updated successfully";
 
-		public static string CategoryAdded = "The category has been added successfully.";
-		public static string CategoryDeleted = "The category has been deleted successfully.";
-		public static string CategoryUpdated = "The category has been updated successfully.";
+		public static string CategoryAdded = "The category has been added successfully";
+		public static string CategoryDeleted = "The category has been deleted successfully";
+		public static string CategoryUpdated = "The category has been updated successfully";
 
-
+		public static string UserNotFound = "User not found";
 	}
 }
