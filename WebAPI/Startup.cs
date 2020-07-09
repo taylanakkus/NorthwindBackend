3533a94c81ba617cc40d06d47a40c2d4356024a3
t4yo <taylan.akkus@outlook.com>
2020-07-09 14:39:40 +0300

    13-17. ServiceCollection Extension

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Extensions;
+using Core.Utilities.DependencyResolvers;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -18,78 +21,82 @@ using Microsoft.OpenApi.Models;
 
 namespace WebAPI
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-    public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddControllers();
-      services.AddCors(options =>
-      {
-        options.AddPolicy("AlloworOrigin",
-          builder => builder.WithOrigins("http://localhost:3000")
-          ); ;
-      });
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddCors(options =>
+			{
+				options.AddPolicy("AlloworOrigin",
+			builder => builder.WithOrigins("http://localhost:3000")
+			); ;
+			});
 
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-      });
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+			});
 
-      var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+			var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
-        options =>
-        {
-          options.TokenValidationParameters = new TokenValidationParameters
-          {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = tokenOptions.Issuer,
-            ValidAudience = tokenOptions.Audience,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
-          };
-        });
-    }
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+			  options =>
+			  {
+				  options.TokenValidationParameters = new TokenValidationParameters
+				  {
+					  ValidateIssuer = true,
+					  ValidateAudience = true,
+					  ValidateLifetime = true,
+					  ValidIssuer = tokenOptions.Issuer,
+					  ValidAudience = tokenOptions.Audience,
+					  ValidateIssuerSigningKey = true,
+					  IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+				  };
+			  });
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
+			services.AddDependencyResolvers(new ICoreModule[] {
+				new CoreModule()
+			});
+		}
 
-      app.UseHttpsRedirection();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-      app.UseRouting();
+			app.UseHttpsRedirection();
 
-      app.UseAuthentication();
+			app.UseRouting();
 
-      app.UseAuthorization();
+			app.UseAuthentication();
 
-      app.UseCors(
-        builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader()
-        );
-      app.UseSwagger();
-      app.UseSwaggerUI(c =>
-      {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
-      });
+			app.UseAuthorization();
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
-    }
-  }
+			app.UseCors(
+			  builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader()
+			  );
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger");
+			});
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
